b1972e92352606aebb0fe2e3a513d097576e3aae
Stanisław Puławski <Stanisaw Puawski>
2017-03-11 03:25:11 +0100

    ListOfObjects ready

---
+++ BindingProject/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BindingProject
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            InitBinding();
+        }
+
+        private void InitBinding()
+        {
+            Binding binding = new Binding();
+        }
+
+
+        //class Human
+        //{
+        //    public Human() { }
+
+        //    public Human(int _Id, int _Age, string _Name, string _Surname)
+        //    {
+        //        Id = _Id;
+        //        Age = _Age;
+        //        Name = _Name;
+        //        Surname = _Surname;
+        //    }
+
+
+
+
+        //    public int Id { get; set; }
+        //    public int Age { get; set; }
+        //    public string Name { get; set; }
+        //    public string Surname { get; set; }
+        //}
+
+
+
+    }
+
+}
---
+++ ListOfObjects/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ListOfObjects
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        List<Human> ListOfHumans = new List<Human>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Human tempHuman = new Human();
+            tempHuman.Age = Convert.ToInt32(tbAge.Text);
+            tempHuman.Id = (ListOfHumans.Count+1);
+            tempHuman.Name = Convert.ToString(tbName.Text);
+            tempHuman.Surname = Convert.ToString(tbSurname.Text);
+
+            ListOfHumans.Add(tempHuman);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            cbSelect.Items.Clear();
+            foreach (var item in ListOfHumans)
+            {
+                cbSelect.Items.Add($"{item.Id} - {item.Surname}");
+            }
+        }
+
+        private void cbSelect_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int selected = cbSelect.SelectedIndex;
+            if(selected != -1)
+                {
+
+            tbAge2.Text = ListOfHumans[selected].Age.ToString();
+            tbName2.Text = ListOfHumans[selected].Name.ToString();
+            tbSurname2.Text = ListOfHumans[selected].Surname.ToString();
+            }
+
+
+        }
+    }
+
+    class Human
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+
+}
